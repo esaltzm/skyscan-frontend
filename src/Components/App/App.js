c7a12e4ad78aaea167a04ba4b390a0599d10ee70
esaltzm <elisaltzman@gmail.com>
2023-01-03 15:03:57 -0500

    display mapbox

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Map from '../Map/Map'
 import './App.css';
 
 export default function App() {
 	return (
 		<div className='App'>
-			<h1>SkyScan</h1>
+			<Map />
 		</div>
 	)
 }
