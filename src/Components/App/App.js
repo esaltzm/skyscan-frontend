839379a379246fe80ef4afc9dd80a87119e51e2f
esaltzm <elisaltzman@gmail.com>
2023-01-04 20:00:19 -0500

    alternative to plotly

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
 import Mapbox from '../Mapbox/Mapbox'
 import DataLayer from '../DataLayer/DataLayer'
+import DataMap from '../DataMap/DataMap'
 import './App.css';
 
 export default function App() {
 	const [bounds, setBounds] = useState(null)
 	return (
 		<div className='App'>
-			<Mapbox setBounds={setBounds}></Mapbox>
-			<DataLayer />
+			{/* <Mapbox setBounds={setBounds}></Mapbox>
+			<DataLayer /> */}
+			<DataMap />
 		</div>
 	)
 }
